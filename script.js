314f59e1fcf46954714f795e2bff311f662640c5
kaled <kaledbit@gmail.com>
2023-03-09 06:28:11 -0600

    Uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+function ul() {
+    let v = document.getElementById('ul');
+    v.style.display = 'block';
+}
+function ul2() {
+    let v = document.getElementById('ul');
+    v.style.display = 'none';
+}
